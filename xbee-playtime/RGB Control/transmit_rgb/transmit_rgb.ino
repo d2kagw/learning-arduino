b4af607f322e02c9dff89355e4384673570752fd
Aaron Wallis <aaron.wallis@gmail.com>
2013-04-04 17:38:23 +1100

    rewrite of the transmitter - it works!

--- xbee-playtime/RGB Control/transmit_rgb/transmit_rgb.ino
+++ xbee-playtime/RGB Control/transmit_rgb/transmit_rgb.ino
@@ -5,35 +5,36 @@ int ledR = 11;
 int ledG = 9;
 int ledB = 10;
 
-float r = 0;
+float r = 0.5;
 float b = 0;
 float g = 0;
 
-String rgbData = "";
+String d1;
+int rData, gData, bData;
 
 void setup() {
-//  pinMode(ledR, OUTPUT);
-//  pinMode(ledG, OUTPUT);
-//  pinMode(ledB, OUTPUT);
-
-//  Serial.begin(19200);
+  Serial.begin(19200);
   mySerial.begin(19200);
 }
 
-void loop()
-{
-//  analogWrite(ledR, abs(sin(r)*255.0));
-//  analogWrite(ledG, abs(sin(g)*255.0));
-//  analogWrite(ledB, abs(sin(b)*255.0));
-  rgbData = String("a");
-//  rgbData = rgbData + (int)abs(sin(r)*255.0) + "," + (int)abs(sin(g)*255.0) + "," + (int)abs(sin(b)*255.0);
-
-//  Serial.println(rgbData);
-  mySerial.print(rgbData);
-
+void loop() {
   r = r + 0.01;
   g = g + 0.02;
   b = b + 0.03;
 
-  delay(100);
+  rData = (int)abs(sin(r)*255.0);
+  gData = (int)abs(sin(g)*255.0);
+  bData = (int)abs(sin(b)*255.0);
+
+  d1  = String(rData);
+  d1 += ",";
+  d1 += String(gData);
+  d1 += ",";
+  d1 += String(bData);
+  d1 += "e";
+
+  Serial.println(d1);
+  mySerial.print(d1);
+
+  delay(5);
 }
